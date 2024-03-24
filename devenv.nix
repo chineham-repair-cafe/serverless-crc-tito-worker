2ff2652526d5ebdce51ee214b9df2ad97a4fac12
Dom Rodriguez <shymega@shymega.org.uk>
2024-03-24 00:05:23 +0000

    feat(devenv): Add `devenv` support

---
+++ devenv.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  packages = [ pkgs.git pkgs.wrangler ];
+
+  languages = {
+    nix.enable = true;
+    rust.enable = true;
+    javascript.enable = true;
+    shell.enable = true;
+  };
+
+  devcontainer.enable = true;
+  difftastic.enable = true;
+  dotenv.enable = true;
+
+  pre-commit.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    actionlint.enable = true;
+    nixpkgs-fmt.enable = true;
+    markdownlint.enable = true;
+    statix.enable = true;
+  };
+  pre-commit.settings.clippy.offline = false;
+}
